3f4d8ef5904ec63ab19bc70d7016f5f93ba61cf8
2301_76348333 <328753359@qq.com>
2026-09-19 00:33:48 +0800

    1

---
+++ routes/test/index.jsx
@@ -0,0 +1,14 @@
+import Counter from '../../islands/Counter.tsx';
+import Test from '../../islands/test/Test.jsx';
+import { useSignal } from '@preact/signals';
+
+export default () => {
+	const count = useSignal(3);
+	return (
+		<div>
+			<Counter count={count} />
+			<div>------</div>
+			<Test />
+		</div>
+	);
+};
