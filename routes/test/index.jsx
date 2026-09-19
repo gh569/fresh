dcf19502ea8216bcb2f45c4ed6b5e7ebe8f35253
2301_76348333 <328753359@qq.com>
2026-09-19 11:47:54 +0800

    1

--- routes/test/index.jsx
+++ routes/test/index.jsx
@@ -1,14 +1,22 @@
-import Counter from '../../islands/Counter.tsx';
-import Test from '../../islands/test/Test.jsx';
 import { useSignal } from '@preact/signals';
+import Test from '../../islands/test/Test.jsx';
+import { Head } from "fresh/runtime";
 
 export default () => {
-	const count = useSignal(3);
-	
+	const scriptCode = `
+	    import m from "https://cdn.jsdelivr.net/gh/gh569/fresh@main/static/data.js";
+	    console.log("拿到数据", m);
+	  `;
 	return (
+	
 		<div>
-			<Counter count={count} />
-			<div>------</div>
+		<head>
+		<script type="module" dangerouslySetInnerHTML={{ __html: scriptCode }} />
+		<script type='module' src='/test.js'/>
+		</head>
+		
+			<button id='div'>点击这里</button>
+			
 			<Test />
 		</div>
 	);
---
+++ static/test.js
@@ -0,0 +1,5 @@
+
+ import m from "https://cdn.jsdelivr.net/gh/gh569/fresh@main/static/data.js";
+document.getElementById('div').onclick=function(){
+	this.innerText=JSON.stringify(m)
+}
